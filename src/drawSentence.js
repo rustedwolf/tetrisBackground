b5edf3cf4caea0e863203bd5fc877aa14d5035f3
Ryszard <rustedwolf@gmail.com>
2015-04-06 16:09:36 +0300

    Update on 0.2.1 - Git ignore update - Returned code back to working state

---
+++ src/drawSentence.js
@@ -0,0 +1,211 @@
+/*
+ * A section dedicated to my girlfriend
+ */
+
+// Kocham Cię, Kociu!
+
+var letters = {
+    0: {
+        w: 5,
+        data: [
+            [0, 0],
+            [4, 0],
+            [0, 1],
+            [1, 1],
+            [3, 1],
+            [4, 1],
+            [0, 2],
+            [2, 2],
+            [4, 2],
+            [0, 3],
+            [4, 3],
+            [0, 4],
+            [4, 4]
+        ]
+    },
+    1: {
+        w: 4,
+        data: [
+            [0, 0],
+            [3, 0],
+            [0, 1],
+            [2, 1],
+            [0, 2],
+            [1, 2],
+            [0, 3],
+            [2, 3],
+            [0, 4],
+            [3, 4]
+        ]
+    },
+    2: {
+        w: 4,
+        data: [
+            [0, 0],
+            [1, 0],
+            [2, 0],
+            [3, 0],
+            [0, 1],
+            [0, 2],
+            [1, 2],
+            [0, 3],
+            [0, 4],
+            [1, 4],
+            [2, 4],
+            [3, 4],
+            [2, 5],
+            [3, 4]
+        ]
+    },
+    3: {
+        w: 4,
+        data: [
+            [1, 0],
+            [2, 0],
+            [0, 1],
+            [3, 1],
+            [0, 2],
+            [1, 2],
+            [2, 2],
+            [3, 2],
+            [0, 3],
+            [3, 3],
+            [0, 4],
+            [3, 4]
+        ]
+    },
+    4: {
+        w: 4,
+        data: [
+            [0, 0],
+            [3, 0],
+            [0, 1],
+            [3, 1],
+            [0, 2],
+            [1, 2],
+            [2, 2],
+            [3, 2],
+            [0, 3],
+            [3, 3],
+            [0, 4],
+            [3, 4]
+        ]
+    },
+    5: {
+        w: 4,
+        data: [
+            [0, 0],
+            [3, 0],
+            [0, 1],
+            [3, 1],
+            [0, 2],
+            [3, 2],
+            [0, 3],
+            [3, 3],
+            [1, 4],
+            [2, 4]
+        ]
+    },
+    6: {
+        w: 1,
+        data: [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [0, 4]
+        ]
+    },
+    7: {
+        w: 1,
+        data: [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [0, 4]
+        ]
+    },
+    8: {
+        w: 4,
+        data: [
+            [1, 0],
+            [2, 0],
+            [0, 1],
+            [3, 1],
+            [0, 2],
+            [0, 3],
+            [3, 3],
+            [1, 4],
+            [2, 4]
+        ]
+    },
+    9: {
+        w: 4,
+        data: [
+            [1, 0],
+            [2, 0],
+            [0, 1],
+            [3, 1],
+            [0, 2],
+            [3, 2],
+            [0, 3],
+            [3, 3],
+            [1, 4],
+            [2, 4]
+        ]
+    },
+    10: {
+        w: 2,
+        data: [
+            [0, 4],
+            [1, 4],
+            [1, 5]
+        ]
+    }
+};
+var fistLine = [1, 9, 8, 4, 3, 0],
+    secondLine = [8, 6, 2, 10],
+    thirdLine = [1, 9, 8, 6, 5, 7];
+
+var delay = 7000;
+var yOffset = 10;
+
+function drawSentence(array) {
+    var width = 0;
+
+    array.forEach(function(letter) {
+        width += letters[letter].w + 2;
+    });
+
+    if (width > 0) {
+        width -= 2;
+    }
+
+    var xOffset = Math.floor((xBlocks - width) / 2);
+
+    array.forEach(function(letter) {
+        letters[letter].data.forEach(function(blockInfo) {
+            var x = xOffset;
+            var y = yOffset;
+            setTimeout(function() {
+                var block = generateBlock(blockSizeSpaced, blockInfo, (blockSizeSpaced * x), (blockSizeSpaced * y));
+                block.attr({
+                    'opacity': 0
+                });
+                block.animate({
+                    'opacity': 1
+                }, 1000);
+            }, delay);
+            delay += 100;
+        });
+        xOffset = xOffset + letters[letter].w + 2;
+    });
+
+    yOffset += 8;
+}
+
+drawSentence(fistLine);
+drawSentence(secondLine);
+drawSentence(thirdLine);
+
+// Endof: Kocham Cię, Kociu!
--- src/raphael.tertrisBackground.js
+++ src/raphael.tertrisBackground.js
@@ -1,224 +1,97 @@
-function TetrisBackgroud(target){	
-	var targetObj = document.getElementById(target);
-	this.paper = Raphael(target, targetObj.offsetWidth, targetObj.offsetHeight);
-	this.shapeCount = 7;
-    this.blockSize = 10;
-    this.spacing = 1;
-    this.fallingSpeed = 400;
-    this.blockSizeSpaced = blockSize + spacing;
-    this.blockColor = '#17bee3';
-	
-    this.gridWidth = Math.floor(targetObj.offsetWidth / blockSizeSpaced);
-    this.gridHeight = Math.floor(targetObj.offsetHeight / blockSizeSpaced);
-	this.grid = [new Array(this.gridWidth), new Array(this.gridHeight)];
-	
-	this.prepareContainer = function(){
-		var yZone = Math.floor(yBlocks * 0.9),
-			xZone = Math.floor(xBlocks / 2);
-		
-		// Fill the paper with base blocks
-		for (var x = 0; x <= xBlocks; x++) {
-		
-			blockPlacement[x] = Array(yBlocks);
-
-			for (var y = fillPart; y <= yBlocks; y++) {
-				var chance = (Math.random() * 101),
-					xChance = 0,
-					yChance = 0;
-				yChance = (0 !== y) ? ((y - fillPart) * 100) / (yBlocks + 1 - fillPart) : 0;
-				if (x <= xHalf) {
-					xChance = (0 !== x) ? (x * 100) / xHalf : 0;
-				} else {
-					xChance = ((xBlocks - x + 1) * 100) / xHalf;
-				}
-
-				var maxChance = (xChance + yChance) * 0.8;
-
-				//R.text((blockSizeSpaced * x) + blockSizeSpaced / 2, (blockSizeSpaced * y) + blockSizeSpaced / 2, x);
-				if (chance < maxChance) {
-					blockPlacement[x][y] = true;
-					generateBlock(blockSizeSpaced, blockInfo, (blockSizeSpaced * x), (blockSizeSpaced * y));
-					//R.text((blockSizeSpaced * x) + blockSizeSpaced / 2, (blockSizeSpaced * y) + blockSizeSpaced / 2, y);
-				} else {
-					blockPlacement[x][y] = false;
-				}
-			}
-		}
-	};
-	
-	this.drawBlock = function(blockSize, xPosition, yPosition) {		
-		var block = this.paper.rect(xPosition, yPosition, blockSize, blockSize);
-		block.attr({
-			fill: this.blockColor,
-			'stroke-width': 0,
-			'stroke-linejoin': 'miter'
-		});
-		return block;
-	};
-	
-	this.prepareContainer();
-}
+// Options
+var shapeCount = 7;
+var blockTrueSize = 9;
+var spacing = 1;
+var fallSpeed = 400; // less is faster
+var blockSize = blockTrueSize + spacing;
+var blockColor = '#17bee3';
+var target = 'animationContainer';
+var blockPlacement = Array();
 
 // Other Globals
-var targetObj = document.getElementById(this.targetContainer),
-    xBlocks = Math.floor(targetObj.offsetWidth / blockSizeSpaced),
-    yBlocks = Math.floor(targetObj.offsetHeight / blockSizeSpaced),
-    containerWidth = xBlocks * blockSizeSpaced,
-    containerHeight = yBlocks * blockSizeSpaced;
+var targetObj = document.getElementById(target);
+var xBlocks = Math.floor(targetObj.offsetWidth / blockSize);
+var yBlocks = Math.floor(targetObj.offsetHeight / blockSize);
+var containerWidth = xBlocks * blockSize;
+var containerHeight = yBlocks * blockSize;
 
-var R = Raphael(this.targetContainer, containerWidth, containerHeight);
+var R = Raphael(target, '100%', '100%');
 
 var shapesData = {
-    'I': {
-        size: 4,
-        blocks: Array(
-        [
-            [0, 1],
-            [1, 1],
-            [2, 1],
-            [3, 1]
-        ], [
-            [2, 0],
-            [2, 1],
-            [2, 2],
-            [2, 3]
-        ])
-    },
-        'T': {
-        size: 3,
-        blocks: Array(
-        [
-            [0, 1],
-            [1, 1],
-            [2, 1],
-            [1, 2]
-        ], [
-            [1, 0],
-            [0, 1],
-            [1, 1],
-            [1, 2]
-        ], [
-            [1, 1],
-            [0, 2],
-            [1, 2],
-            [2, 2]
-        ], [
-            [1, 0],
-            [1, 1],
-            [2, 1],
-            [1, 2]
-        ])
-    },
-        'L': {
-        size: 3,
-        blocks: Array(
-        [
-            [0, 1],
-            [1, 1],
-            [2, 1],
-            [0, 2]
-        ], [
-            [0, 0],
-            [1, 0],
-            [1, 1],
-            [1, 2]
-        ], [
-            [2, 1],
-            [0, 2],
-            [1, 2],
-            [2, 2]
-        ], [
-            [1, 0],
-            [1, 1],
-            [1, 2],
-            [2, 2]
-        ])
-    },
-        'J': {
-        size: 3,
-        blocks: Array(
-        [
-            [0, 1],
-            [1, 1],
-            [2, 1],
-            [2, 2]
-        ], [
-            [1, 0],
-            [1, 1],
-            [0, 2],
-            [1, 2]
-        ], [
-            [0, 1],
-            [0, 2],
-            [1, 2],
-            [2, 2]
-        ], [
-            [1, 0],
-            [2, 0],
-            [1, 1],
-            [1, 2]
-        ])
-    },
-        'S': {
-        size: 3,
-        blocks: Array(
-        [
-            [1, 1],
-            [2, 1],
-            [0, 2],
-            [1, 2]
-        ], [
-            [0, 0],
-            [0, 1],
-            [1, 1],
-            [1, 2]
-        ])
-    },
-        'Z': {
-        size: 3,
-        blocks: Array(
-        [
-            [0, 1],
-            [1, 1],
-            [1, 2],
-            [2, 2]
-        ], [
-            [2, 0],
-            [1, 1],
-            [2, 1],
-            [1, 2]
-        ])
-    },
-        'O': {
-        size: 2,
-        blocks: Array(
-        [
-            [0, 0],
-            [1, 0],
-            [0, 1],
-            [1, 1]
-        ])
-    }
+	'I': {
+		size: 4,
+		blocks: Array(
+				[[0, 1], [1, 1], [2, 1], [3, 1]],
+				[[2, 0], [2, 1], [2, 2], [2, 3]]
+				)
+	},
+	'T': {
+		size: 3,
+		blocks: Array(
+				[[0, 1], [1, 1], [2, 1], [1, 2]],
+				[[1, 0], [0, 1], [1, 1], [1, 2]],
+				[[1, 1], [0, 2], [1, 2], [2, 2]],
+				[[1, 0], [1, 1], [2, 1], [1, 2]]
+				)
+	},
+	'L': {
+		size: 3,
+		blocks: Array(
+				[[0, 1], [1, 1], [2, 1], [0, 2]],
+				[[0, 0], [1, 0], [1, 1], [1, 2]],
+				[[2, 1], [0, 2], [1, 2], [2, 2]],
+				[[1, 0], [1, 1], [1, 2], [2, 2]]
+				)
+	},
+	'J': {
+		size: 3,
+		blocks: Array(
+				[[0, 1], [1, 1], [2, 1], [2, 2]],
+				[[1, 0], [1, 1], [0, 2], [1, 2]],
+				[[0, 1], [0, 2], [1, 2], [2, 2]],
+				[[1, 0], [2, 0], [1, 1], [1, 2]]
+				)
+	},
+	'S': {
+		size: 3,
+		blocks: Array(
+				[[1, 1], [2, 1], [0, 2], [1, 2]],
+				[[0, 0], [0, 1], [1, 1], [1, 2]]
+				)
+	},
+	'Z': {
+		size: 3,
+		blocks: Array(
+				[[0, 1], [1, 1], [1, 2], [2, 2]],
+				[[2, 0], [1, 1], [2, 1], [1, 2]]
+				)
+	},
+	'O': {
+		size: 2,
+		blocks: Array(
+				[[0, 0], [1, 0], [0, 1], [1, 1]]
+				)
+	}
 };
 
 /**
  * Generates the block on certain paper position
  *
- * @param {int} blockSizeSpaced complear shape size
+ * @param {int} blockSize complear shape size
  * @param {array} blockInfo Block sizes
  * @param {int} xOffset X-axis position
  * @param {int} yOffset Y-axis postion
  * @returns {object} Raphalel object, the block itself
  */
-function generateBlock(blockSizeSpaced, blockInfo, xOffset, yOffset) {
+function generateBlock(blockSize, blockInfo, xOffset, yOffset) {
 
-    var block = R.rect(blockSizeSpaced * blockInfo[0] + xOffset, blockSizeSpaced * blockInfo[1] + yOffset, blockSize, blockSize)
-        .attr({
-        fill: blockColor,
-            "stroke-width": 0,
-            'stroke-linejoin': 'miter'
-    });
-    return block;
+	var block = R.rect(blockSize * blockInfo[0] + xOffset, blockSize * blockInfo[1] + yOffset, blockTrueSize, blockTrueSize)
+			.attr({
+				fill: blockColor,
+				"stroke-width": 0,
+				'stroke-linejoin': 'miter'
+			});
+	return block;
 }
 
 /**
@@ -230,36 +103,37 @@ function generateBlock(blockSizeSpaced, blockInfo, xOffset, yOffset) {
  * @returns {object} shape
  */
 function generateShape(shapeInfo) {
-    var rotate = Math.floor(Math.random() * shapeInfo.blocks.length);
+	var rotate = Math.floor(Math.random() * shapeInfo.blocks.length);
 
-    var shape = {
-        size: shapeInfo.size,
-        rotate: rotate,
-        blocks: shapeInfo.blocks[rotate],
-        faling: false,
-        xOffset: 0,
-        yOffset: 0,
-        objects: R.set()
-    };
+	var shape = {
+		size: shapeInfo.size,
+		rotate: rotate,
+		blocks: shapeInfo.blocks[rotate],
+		faling: false,
+		xOffset: 0,
+		yOffset: 0,
+		objects: R.set()
+	};
 
-    var blocks = shape.blocks,
-        random = Math.floor(Math.random() * (containerWidth / blockSizeSpaced - shape.size)),
-        posX = random * blockSizeSpaced,
-        posY = (rotate % 2 !== 0) ? 0 : -(blockSizeSpaced);
+	var blocks = shape.blocks,
+			random = Math.floor(Math.random() * (containerWidth / blockSize - shape.size)),
+			posX = random * blockSize,
+			posY = (rotate % 2 !== 0) ? 0 : -(blockSize);
 
-    shape.xOffset = random;
-    if (rotate % 2 !== 0) shape.yOffset = -1;
+	shape.xOffset = random;
+	if (rotate % 2 !== 0)
+		shape.yOffset = -1;
 
-    if (shape.size === 2) {
-        posX += blockSizeSpaced;
-        posY += blockSizeSpaced;
-    }
+	if (shape.size === 2) {
+		posX += blockSize;
+		posY += blockSize;
+	}
 
-    for (var j in shape.blocks) {
-        shape.objects.push(generateBlock(blockSizeSpaced, blocks[j], posX, posY));
-    }
+	for (var j in shape.blocks) {
+		shape.objects.push(generateBlock(blockSize, blocks[j], posX, posY));
+	}
 
-    return shape;
+	return shape;
 }
 
 /**
@@ -271,86 +145,86 @@ function generateShape(shapeInfo) {
  */
 function dropShape(shape, speed) {
 
-    var addY = (shape.rotate % 2 !== 0) ? 1 : 0;
-    var addX = 0;
-    if (shape.size === 2) {
-        addY = 1;
-        addX = 1;
-    }
+	var addY = (shape.rotate % 2 !== 0) ? 1 : 0;
+	var addX = 0;
+	if (shape.size === 2) {
+		addY = 1;
+		addX = 1;
+	}
 
-    var keepFaling = true;
-    shape.blocks.forEach(function (p) {
-        if (keepFaling) {
-            if (undefined !== blockPlacement[p[0] + shape.xOffset]) {
-                keepFaling = (!blockPlacement[p[0] + shape.xOffset + addX][p[1] + shape.yOffset + addY]);
-                if (p[1] + shape.yOffset + addY >= yBlocks) {
-                    keepFaling = false;
-                }
-            } else {
-                keepFaling = !(p[1] >= yBlocks);
-            }
-        }
-    });
+	var keepFaling = true;
+	shape.blocks.forEach(function(p) {
+		if (keepFaling) {
+			if (undefined !== blockPlacement[p[0] + shape.xOffset]) {
+				keepFaling = (!blockPlacement[p[0] + shape.xOffset + addX][p[1] + shape.yOffset + addY]);
+				if (p[1] + shape.yOffset + addY >= yBlocks) {
+					keepFaling = false;
+				}
+			} else {
+				keepFaling = !(p[1] >= yBlocks);
+			}
+		}
+	});
 
-    if (keepFaling) {
-        shape.yOffset = shape.yOffset + 1;
-        if (!shape.faling) {
-            shape.faling = true;
-            shape.objects.animate({
-                transform: 't0,' + (shape.yOffset * blockSizeSpaced)
-            }, speed / 2, '<>', function () {
-                shape.faling = false;
-                setTimeout(function () {
-                    return dropShape(shape, speed);
-                }, speed);
-            });
-        }
-    } else {
-        shape.objects.animate({
-            opacity: 0
-        }, 1000, '<>', function () {
-            shape.objects.remove();
-            shape.objects = null;
-            shape = null;
-            timeoutDrop();
-        });
-    }
+	if (keepFaling) {
+		shape.yOffset = shape.yOffset + 1;
+		if (!shape.faling) {
+			shape.faling = true;
+			shape.objects.animate({
+				transform: 't0,' + (shape.yOffset * blockSize)
+			}, speed / 2, '<>', function() {
+				shape.faling = false;
+				setTimeout(function() {
+					return dropShape(shape, speed);
+				}, speed);
+			});
+		}
+	} else {
+		shape.objects.animate({
+			opacity: 0
+		}, 1000, '<>', function() {
+			shape.objects.remove();
+			shape.objects = null;
+			shape = null;
+			timeoutDrop();
+		});
+	}
 }
 
 /**
  * Adds random bottom blocks
  */
 function fillBottom() {
-    var fillPart = Math.floor(yBlocks * 0.9),
-        xHalf = Math.floor(xBlocks / 2),
-        blockInfo = [0, 0];
-    // Fill the paper with base blocks
-    for (var x = 0; x <= xBlocks; x++) {
-        blockPlacement[x] = Array(yBlocks);
+	var fillPart = Math.floor(yBlocks * 0.9),
+			xHalf = Math.floor(xBlocks / 2),
+			blockInfo = [0, 0];
+	// Fill the paper with base blocks
+	for (var x = 0; x <= xBlocks; x++) {
+		blockPlacement[x] = Array(yBlocks);
 
-        for (var y = fillPart; y <= yBlocks; y++) {
-            var chance = (Math.random() * 101),
-                xChance = 0,
-                yChance = 0;
-            yChance = (0 !== y) ? ((y - fillPart) * 100) / (yBlocks + 1 - fillPart) : 0;
-            if (x <= xHalf) {
-                xChance = (0 !== x) ? (x * 100) / xHalf : 0;
-            } else {
-                xChance = ((xBlocks - x + 1) * 100) / xHalf;
-            }
+		for (var y = fillPart; y <= yBlocks; y++) {
+			var chance = (Math.random() * 101),
+					xChance = 0,
+					yChance = 0;
+			yChance = (0 !== y) ? ((y - fillPart) * 100) / (yBlocks + 1 - fillPart) : 0;
+			if (x <= xHalf) {
+				xChance = (0 !== x) ? (x * 100) / xHalf : 0;
+			} else {
+				xChance = ((xBlocks - x + 1) * 100) / xHalf;
+			}
 
-            var maxChance = (xChance + yChance) * 0.8;
+			var maxChance = (xChance + yChance) * 0.8;
 
-            //R.text((blockSizeSpaced * x) + blockSizeSpaced / 2, (blockSizeSpaced * y) + blockSizeSpaced / 2, x);
-            if (chance < maxChance) {
-                blockPlacement[x][y] = true;
-                generateBlock(blockSizeSpaced, blockInfo, (blockSizeSpaced * x), (blockSizeSpaced * y));
-                //R.text((blockSizeSpaced * x) + blockSizeSpaced / 2, (blockSizeSpaced * y) + blockSizeSpaced / 2, y);
-            } else {
-                blockPlacement[x][y] = false;
-            }
-        }
-    }
+			//R.text((blockSize * x) + blockSize / 2, (blockSize * y) + blockSize / 2, x);
+			if (chance < maxChance) {
+				blockPlacement[x][y] = true;
+				generateBlock(blockSize, blockInfo, (blockSize * x), (blockSize * y));
+				//R.text((blockSize * x) + blockSize / 2, (blockSize * y) + blockSize / 2, y);
+			} else {
+				blockPlacement[x][y] = false;
+			}
+		}
+	}
 
 }
 
@@ -358,14 +232,14 @@ function fillBottom() {
  * Calls the shape dropping fn in random time
  */
 function timeoutDrop() {
-    var delay = Math.random() * 24000;
-    setTimeout(function () {
-        var keys = Object.keys(shapesData),
-            keyPos = Math.floor((Math.random() * Object.keys(shapesData).length)),
-            shape = generateShape(shapesData[keys[keyPos]]);
+	var delay = Math.random() * 24000;
+	setTimeout(function() {
+		var keys = Object.keys(shapesData),
+				keyPos = Math.floor((Math.random() * Object.keys(shapesData).length)),
+				shape = generateShape(shapesData[keys[keyPos]]);
 
-        dropShape(shape, fallingSpeed);
-    }, delay);
+		dropShape(shape, fallSpeed);
+	}, delay);
 }
 
 /**
@@ -374,218 +248,17 @@ function timeoutDrop() {
  * @returns {function} returns the interval loop
  */
 function dropShapes() {
-    for (var i = 0; i < shapeCount; i++) {
-        timeoutDrop();
-    }
+	for (var i = 0; i < shapeCount; i++) {
+		timeoutDrop();
+	}
 }
 
 fillBottom();
 var interval = dropShapes();
 
-// Kocham Ci? Kociu!
 
-var letters = {
-    0: {
-        w: 5,
-        data: [
-            [0, 0],
-            [4, 0],
-            [0, 1],
-            [1, 1],
-            [3, 1],
-            [4, 1],
-            [0, 2],
-            [2, 2],
-            [4, 2],
-            [0, 3],
-            [4, 3],
-            [0, 4],
-            [4, 4]
-        ]
-    },
-    1: {
-        w: 4,
-        data: [
-            [0, 0],
-            [3, 0],
-            [0, 1],
-            [2, 1],
-            [0, 2],
-            [1, 2],
-            [0, 3],
-            [2, 3],
-            [0, 4],
-            [3, 4]
-        ]
-    },
-    2: {
-        w: 4,
-        data: [
-            [0, 0],
-            [1, 0],
-            [2, 0],
-            [3, 0],
-            [0, 1],
-            [0, 2],
-            [1, 2],
-            [0, 3],
-            [0, 4],
-            [1, 4],
-            [2, 4],
-            [3, 4],
-            [2, 5],
-            [3, 4]
-        ]
-    },
-    3: {
-        w: 4,
-        data: [
-            [1, 0],
-            [2, 0],
-            [0, 1],
-            [3, 1],
-            [0, 2],
-            [1, 2],
-            [2, 2],
-            [3, 2],
-            [0, 3],
-            [3, 3],
-            [0, 4],
-            [3, 4]
-        ]
-    },
-    4: {
-        w: 4,
-        data: [
-            [0, 0],
-            [3, 0],
-            [0, 1],
-            [3, 1],
-            [0, 2],
-            [1, 2],
-            [2, 2],
-            [3, 2],
-            [0, 3],
-            [3, 3],
-            [0, 4],
-            [3, 4]
-        ]
-    },
-    5: {
-        w: 4,
-        data: [
-            [0, 0],
-            [3, 0],
-            [0, 1],
-            [3, 1],
-            [0, 2],
-            [3, 2],
-            [0, 3],
-            [3, 3],
-            [1, 4],
-            [2, 4]
-        ]
-    },
-    6: {
-        w: 1,
-        data: [
-            [0, 0],
-            [0, 1],
-            [0, 2],
-            [0, 3],
-            [0, 4]
-        ]
-    },
-    7: {
-        w: 1,
-        data: [
-            [0, 0],
-            [0, 1],
-            [0, 2],
-            [0, 4]
-        ]
-    },
-    8: {
-        w: 4,
-        data: [
-            [1, 0],
-            [2, 0],
-            [0, 1],
-            [3, 1],
-            [0, 2],
-            [0, 3],
-            [3, 3],
-            [1, 4],
-            [2, 4]
-        ]
-    },
-    9: {
-        w: 4,
-        data: [
-            [1, 0],
-            [2, 0],
-            [0, 1],
-            [3, 1],
-            [0, 2],
-            [3, 2],
-            [0, 3],
-            [3, 3],
-            [1, 4],
-            [2, 4]
-        ]
-    },
-    10: {
-        w: 2,
-        data: [
-            [0, 4],
-            [1, 4],
-            [1, 5]
-        ]
-    }
-};
-var fistLine = [1, 9, 8, 4, 3, 0],
-    secondLine = [8, 6, 2, 10],
-    thirdLine = [1, 9, 8, 6, 5, 7];
-
-var delay = 7000;
-var yOffset = 10;
-
-function drawSentence(array) {
-    var width = 0;
-
-    array.forEach(function (letter) {
-        width += letters[letter].w + 2;
-    });
-
-    if (width > 0) {
-        width -= 2;
-    }
-
-    var xOffset = Math.floor((xBlocks - width) / 2);
-
-    array.forEach(function (letter) {
-        letters[letter].data.forEach(function (blockInfo) {
-            var x = xOffset;
-            var y = yOffset;
-            setTimeout(function () {
-                var block = generateBlock(blockSizeSpaced, blockInfo, (blockSizeSpaced * x), (blockSizeSpaced * y));
-                block.attr({
-                    'opacity': 0
-                });
-                block.animate({
-                    'opacity': 1
-                }, 1000);
-            }, delay);
-            delay += 100;
-        });
-        xOffset = xOffset + letters[letter].w + 2;
-    });
-
-    yOffset += 8;
+// 1st sequence
+function TetrisBackground(targetContainerId){
+	this.container = getElementById(targetContainerId);
+	this.paper = Raphael(targetContainerId, '100%', '100%');
 }
-
-//drawSentence(fistLine);
-//drawSentence(secondLine);
-//drawSentence(thirdLine);
-
-// Endof: Kocham Ci? Kociu!
