de417670c73fbfa5782717fa20b6d81b76799241
Ryszard <rustedwolf@gmail.com>
2015-06-28 23:51:53 +0300

    Code refactoring - Switched from 4 to 2 character spacing - Function splitted into smaller chunks (some name changes) - Removed some unused or one-time used variables

--- src/raphael.tertrisBackground.js
+++ src/raphael.tertrisBackground.js
@@ -1,220 +1,250 @@
-var grid = [];
-
-var paper, targetObj, xBlockCount, yBlockCount, containerWidth, containerHeight,
-    shapeCount, blockSize, blockSpacing, fallSpeed, blockAreaSize, blockColor;
-
-/**
- * Generates the block on certain paper position
- *
- * @param {int} blockAreaSize complear shape size
- * @param {array} blockInfo Block sizes
- * @param {int} xOffset X-axis position
- * @param {int} yOffset Y-axis postion
- * @returns {object} Raphalel object, the block itself
- */
-function generateBlock(blockAreaSize, blockInfo, xOffset, yOffset) {
-    var block = paper.rect(blockAreaSize * blockInfo[0] + xOffset, blockAreaSize * blockInfo[1] + yOffset, blockSize, blockSize)
-        .attr({
-            fill: blockColor,
-            "stroke-width": 0,
-            'stroke-linejoin': 'miter'
-        });
-    return block;
-}
-
-/**
- * Generates the shape by it's date
- * and placing it above the paper
- * note: needs spaning under random angle
- *
- * @param {object} shapeInfo Object containing shape data
- * @returns {object} shape
- */
-function generateShape(shapeInfo) {
-    var rotate = Math.floor(Math.random() * shapeInfo.blocks.length);
-
-    var shape = {
-        size: shapeInfo.size,
-        rotate: rotate,
-        blocks: shapeInfo.blocks[rotate],
-        faling: false,
-        xOffset: 0,
-        yOffset: 0,
-        objects: paper.set()
-    };
-
-    var blocks = shape.blocks,
-        random = Math.floor(Math.random() * (containerWidth / blockAreaSize - shape.size)),
-        posX = random * blockAreaSize,
-        posY = (rotate % 2 !== 0) ? 0 : -(blockAreaSize);
-
-    shape.xOffset = random;
-    if (rotate % 2 !== 0)
-        shape.yOffset = -1;
-
-    if (shape.size === 2) {
-        posX += blockAreaSize;
-        posY += blockAreaSize;
-    }
-
-    for (var j in shape.blocks) {
-        shape.objects.push(generateBlock(blockAreaSize, blocks[j], posX, posY));
-    }
-
-	return shape;
-}
-
-/**
- * Shape drop sequense
- *
- * @param {type} shape
- * @param {type} speed
- * @returns {undefined}
- */
-function dropShape(shape, speed) {
-    var addY = (shape.rotate % 2 !== 0) ? 1 : 0;
-    var addX = 0;
-    if (shape.size === 2) {
-        addY = 1;
-        addX = 1;
-    }
-
-    var keepFaling = true;
-    shape.blocks.forEach(function(p) {
-        if (keepFaling) {
-            if (undefined !== grid[p[0] + shape.xOffset]) {
-                keepFaling = (!grid[p[0] + shape.xOffset + addX][p[1] + shape.yOffset + addY]);
-                if (p[1] + shape.yOffset + addY >= yBlockCount) {
-                    keepFaling = false;
-                }
-            } else {
-                keepFaling = !(p[1] >= yBlockCount);
-            }
-        }
-    });
-
-    if (keepFaling) {
-        shape.yOffset = shape.yOffset + 1;
-        if (!shape.faling) {
-            shape.faling = true;
-            shape.objects.animate({
-                transform: 't0,' + (shape.yOffset * blockAreaSize)
-            }, speed / 2, '<>', function() {
-                shape.faling = false;
-                setTimeout(function() {
-                    return dropShape(shape, speed);
-                }, speed);
-            });
-        }
-    } else {
-        shape.objects.animate({
-            opacity: 0
-        }, 1000, '<>', function() {
-            shape.objects.remove();
-            shape.objects = null;
-            shape = null;
-            timeoutDrop();
-        });
-    }
-}
-
-/**
- * Calls the shape dropping fn in random time
- */
-function timeoutDrop() {
-    var delay = Math.random() * 24000;
-    setTimeout(function() {
-        var keys = Object.keys(shapesData),
-            keyPos = Math.floor((Math.random() * Object.keys(shapesData).length)),
-            shape = generateShape(shapesData[keys[keyPos]]);
-
-        dropShape(shape, fallSpeed);
-    }, delay);
-}
-
-/**
- * Starts to drop shapes
- *
- * @returns {function} returns the interval loop
- */
-function dropShapes() {
-	for (var i = 0; i < shapeCount; i++) {
-		timeoutDrop();
-	}
-}
-
 function tetrisBackground(options) {
-    initialize(options);
-}
+  var paper, targetObj, xBlockCount, yBlockCount, shapeCount, blockSize,
+    blockSpacing, animationSpeed, blockAreaSize, blockColor, bottomFill;
+  var grid = [];
 
-// Refactoring starts here...
-
-function initialize(options) {
-    // Settings
+  function initialize(options) {
     shapeCount = options.shapeCount || 7;
     blockColor = options.blockColor || '#17bee3';
     blockSize = options.blockSize || 9;
     blockSpacing = options.blockSpacing || 1;
-    fallSpeed = options.animationSpeed || 400; // less is faster
+    animationSpeed = options.animationSpeed || 400; // less is faster
+    bottomFill = options.bottomFill || 0.8;
 
     targetObj = document.getElementById(options.targetId);
     paper = Raphael(options.targetId, '100%', '100%');
 
     blockAreaSize = blockSize + blockSpacing;
+
+    // Generate an empty grid for block tracking
     xBlockCount = Math.ceil(targetObj.offsetWidth / blockAreaSize);
     yBlockCount = Math.ceil(targetObj.offsetHeight / blockAreaSize);
+    generateGrid();
 
-    containerWidth = xBlockCount * blockAreaSize;
-    containerHeight = yBlockCount * blockAreaSize;
-
-	grid = generateGrid();
-
+    // Fill bottom with blocks
     fillBottom();
-    dropShapes();
-}
 
-/**
- * Generates an empty grid for marking block positions
- */
-function generateGrid(){
-	var grid = new Array(xBlockCount);
-	for (var x = 0; x < xBlockCount; x++){
-		grid[x] = new Array(yBlockCount);
-	}
-	return grid;
-}
+    // Spawn a first batch of shapes
+    spawnShapes();
+  }
 
-/**
- * Adds random bottom blocks
- */
-function fillBottom() {
-    var fillPart = Math.floor(yBlockCount * 0.9);
-    var xAxisCenter = Math.round(xBlockCount / 2);
-    var blockInfo = [0, 0];
-
-    // Fill the paper with base blocks
+  /**
+   * Generates an empty grid for marking block positions
+   */
+  function generateGrid() {
+    grid = new Array(xBlockCount);
     for (var x = 0; x < xBlockCount; x++) {
-        for (var y = fillPart; y <= yBlockCount; y++) {
-            var chance = (Math.random() * 101),
-                xChance = 0,
-                yChance = 0;
-            yChance = (0 !== y) ? ((y - fillPart) * 100) / (yBlockCount + 1 - fillPart) : 0;
-            if (x <= xAxisCenter) {
-                xChance = (0 !== x) ? (x * 100) / xAxisCenter : 0;
-            } else {
-                xChance = ((xBlockCount - x + 1) * 100) / xAxisCenter;
-            }
-
-            var maxChance = (xChance + yChance) * 0.8;
-
-            //paper.text((blockAreaSize * x) + blockAreaSize / 2, (blockAreaSize * y) + blockAreaSize / 2, x);
-            if (chance < maxChance) {
-                grid[x][y] = true;
-                generateBlock(blockAreaSize, blockInfo, (blockAreaSize * x), (blockAreaSize * y));
-                //paper.text((blockAreaSize * x) + blockAreaSize / 2, (blockAreaSize * y) + blockAreaSize / 2, y);
-            } else {
-                grid[x][y] = false;
-            }
-        }
+      grid[x] = new Array(yBlockCount);
     }
+  }
+
+  /**
+   * Adds random bottom blocks
+   */
+  function fillBottom() {
+    var yAxisStart = Math.round(yBlockCount * bottomFill);
+    var xAxisCenter = Math.round(xBlockCount / 2);
+
+    for (var x = 0; x < xBlockCount; x++) {
+      for (var y = yAxisStart; y < yBlockCount; y++) {
+        grid[x][y] = placeBlock(xAxisCenter, yAxisStart, x, y);
+        if (grid[x][y]) {
+          generateBlock([0, 0], blockAreaSize * x, blockAreaSize * y);
+        }
+      }
+    }
+  }
+
+  /**
+   * Randomly places a block depending on the block cordiantes
+   * placing is done by "hill" type placing
+   *
+   * @param {int} xAxisCenter center by X-axis
+   * @param {int} yAxisStart start of of the Y-axis
+   * @param {int} x X-axis position
+   * @param {int} y Y-axis position
+   *
+   * @return {boolean}
+   */
+  function placeBlock(xAxisCenter, yAxisStart, x, y) {
+    var chance, maxChance, xChance, yChance;
+    chance = Math.random() * 100;
+
+    if (x <= xAxisCenter) {
+      xChance = (0 !== x) ? (x * 100) / xAxisCenter : 0;
+    } else {
+      xChance = ((xBlockCount - x) * 100) / xAxisCenter;
+    }
+    if (y) {
+      yChance = ((y - yAxisStart) * 100) / (yBlockCount - yAxisStart);
+    } else {
+      yChance = 0;
+    }
+    maxChance = (xChance + yChance) * bottomFill;
+
+    return chance < maxChance;
+  }
+
+  /**
+   * Generates the block on certain paper position
+   *
+   * @param {array} blockInfo Block sizes
+   * @param {int} xOffset X-axis position
+   * @param {int} yOffset Y-axis postion
+   *
+   * @returns {object} Raphalel object, the block itself
+   */
+  function generateBlock(blockInfo, xOffset, yOffset) {
+    var xPos = blockAreaSize * blockInfo[0] + xOffset;
+    var yPos = blockAreaSize * blockInfo[1] + yOffset;
+    var block = paper.rect(xPos, yPos, blockSize, blockSize)
+      .attr({
+        fill: blockColor,
+        "stroke-width": 0,
+        'stroke-linejoin': 'miter'
+      });
+    return block;
+  }
+
+  /**
+   * Spawns a batch of shapes
+   */
+  function spawnShapes() {
+    spawnShape();
+    for (var i = 0; i < shapeCount - 1; i++) {
+      timeoutSpawn();
+    }
+  }
+
+  /**
+   * Spawns a shape at random time
+   */
+  function timeoutSpawn() {
+    setTimeout(function() {
+      spawnShape();
+    }, Math.random() * 24 * 1000);
+  }
+
+  /**
+   * Spawns a shape
+   */
+  function spawnShape() {
+    var shape = generateShape(getRandomShapeCordinates());
+    nextAction(shape);
+  }
+
+  function getRandomShapeCordinates() {
+    var keys = Object.keys(shapesData),
+      keyPos = Math.floor((Math.random() * keys.length));
+    return shapesData[keys[keyPos]];
+  }
+
+  /**
+   * Generates the shape by it's date
+   * and placing it above the paper
+   * note: needs spaning under random angle
+   *
+   * @param {object} shapeInfo Object containing shape data
+   *
+   * @returns {object} shape
+   */
+  function generateShape(shapeInfo) {
+    var rotate = Math.floor(Math.random() * shapeInfo.blocks.length);
+    var random = Math.floor(Math.random() * (xBlockCount - shapeInfo.size));
+    var shape = {
+      size: shapeInfo.size,
+      rotate: rotate,
+      blocks: shapeInfo.blocks[rotate],
+      moving: false,
+      xOffset: random,
+      yOffset: (rotate % 2 !== 0) ? -1 : 0,
+      objects: paper.set()
+    };
+    var posX = random * blockAreaSize;
+    var posY = (rotate % 2 !== 0) ? 0 : -blockAreaSize;
+    if (shape.size === 2) {
+      posX += blockAreaSize;
+      posY += blockAreaSize;
+    }
+
+    for (var j in shape.blocks) {
+      shape.objects.push(generateBlock(shape.blocks[j], posX, posY));
+    }
+
+    return shape;
+  }
+
+  /**
+   * Takes next shape action
+   *
+   * @param {object} shape
+   */
+  function nextAction(shape) {
+    if (canMoveDown(shape)) {
+      moveShapeDown(shape);
+    } else {
+      destroyShape(shape);
+      timeoutSpawn();
+    }
+  }
+
+  /**
+   * Moves shape down by one block
+   *
+   * @param {object} shape
+   */
+  function moveShapeDown(shape) {
+    shape.yOffset = shape.yOffset + 1;
+    if (!shape.moving) {
+      shape.moving = true;
+      shape.objects.animate({
+        transform: 't0,' + (shape.yOffset * blockAreaSize)
+      }, animationSpeed / 2, '<>', function() {
+        shape.moving = false;
+        setTimeout(function() {
+          return nextAction(shape);
+        }, animationSpeed);
+      });
+    }
+  }
+
+  /**
+   * Destroys the shape
+   */
+  function destroyShape(shape) {
+    shape.objects.animate({
+      opacity: 0
+    }, 1000, '<>', function() {
+      shape.objects.remove();
+      shape.objects = null;
+      shape = null;
+    });
+  }
+
+  function canMoveDown(shape) {
+    var canMove = true;
+    var addY = (shape.rotate % 2 !== 0) ? 1 : 0;
+    var addX = 0;
+    if (shape.size === 2) {
+      addY = 1;
+      addX = 1;
+    }
+
+    shape.blocks.forEach(function(p) {
+      if (canMove) {
+        if (undefined !== grid[p[0] + shape.xOffset]) {
+          canMove = (!grid[p[0] + shape.xOffset + addX][p[1] + shape.yOffset + addY]);
+          if (p[1] + shape.yOffset + addY >= yBlockCount) {
+            canMove = false;
+          }
+        } else {
+          canMove = (p[1] < yBlockCount);
+        }
+      }
+    });
+
+    return canMove;
+  }
+
+  initialize(options);
 }
